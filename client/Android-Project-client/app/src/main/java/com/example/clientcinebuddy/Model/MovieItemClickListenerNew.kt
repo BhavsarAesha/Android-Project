15ee02762abd8b6a2a294ff73513f0e7a2cf29e9
unknown <aeshabhavsar12@gmail.com>
2025-08-08 19:50:27 +0530

    Updated admin and client folders

--- client/Android-Project-client/app/src/main/java/com/example/clientcinebuddy/Model/MovieItemClickListenerNew.kt
+++
@@ -1,7 +0,0 @@
-package com.example.clientcinebuddy.Model
-
-import android.widget.ImageView
-
-interface MovieItemClickListenerNew {
-    fun onMovieClick(getVideoDetails: GetVideoDetails, imageView: ImageView)
-}
